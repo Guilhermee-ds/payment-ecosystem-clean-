e28a856ed02fac35add7b7b0c12e2eab4df54529
Guilhermee-ds <contato.guilhermedossantos@gmail.com>
2025-11-21 20:05:16 -0300

    feat: add struct payment

--- api/internal/domain/payment.go
+++ api/internal/domain/payment.go
@@ -1,1 +1,12 @@
 package domain
+
+import "time"
+
+type Payment struct {
+	ID        string    `json:"id"`
+	OrderID   string    `json:"order_id"`
+	Amount    float64   `json:"amount"`
+	Currency  string    `json:"currency"`
+	CardHash  string    `json:"card_hash"`
+	CreatedAt time.Time `json:"created_at"`
+}
